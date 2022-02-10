5ae605f0741cd4fdc998adfd98b026dbddfdf3de
James Whitney <33575806+jameswhitney@users.noreply.github.com>
2022-02-10 16:24:43 -0500

    Refators App to use hooks instead of class based state management

--- src/App.js
+++ src/App.js
@@ -1,51 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import CardList from "./components/cardlist.component";
 import SearchBox from "./components/searchbox.component";
 import Scroll from "./components/scroll.component";
 import Spinner from "./components/spinner.component";
 import ErrorBoundary from "./components/errorboundary.component";
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: [],
-      searchField: "",
-    };
-  }
+function App() {
+  const [robots, setRobots] = useState([]);
+  const [searchField, setSearchField] = useState("");
 
-  componentDidMount() {
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((resp) => resp.json())
       .then((robots) => {
-        return this.setState({ robots: robots });
+        return setRobots(robots);
       })
       .catch((error) => console.log(error));
-  }
+  }, []);
 
-  onSearchChange = (event) => {
-    return this.setState({ searchField: event.target.value });
+  const onSearchChange = (event) => {
+    return setSearchField(event.target.value);
   };
 
-  render() {
-    const { robots, searchField } = this.state;
-    const filteredRobots = robots.filter((robot) => {
-      return robot.name.toLowerCase().includes(searchField.toLocaleLowerCase());
-    });
-    return !robots.length ? (
-      <Spinner />
-    ) : (
-      <div className="tc">
-        <h1>Robofriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <Scroll>
-          <ErrorBoundary>
-            <CardList robots={filteredRobots} />
-          </ErrorBoundary>
-        </Scroll>
-      </div>
-    );
-  }
+  const filteredRobots = robots.filter((robot) => {
+    return robot.name.toLowerCase().includes(searchField.toLocaleLowerCase());
+  });
+  return !robots.length ? (
+    <Spinner />
+  ) : (
+    <div className="tc">
+      <h1>Robofriends</h1>
+      <SearchBox searchChange={onSearchChange} />
+      <Scroll>
+        <ErrorBoundary>
+          <CardList robots={filteredRobots} />
+        </ErrorBoundary>
+      </Scroll>
+    </div>
+  );
 }
 
 export default App;
